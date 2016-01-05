219d3f46043acb3f7983a8ba34379f84bd15bf95
Owain Williams <owain@vouchercloud.com>
2016-01-05 17:29:46 +0000

    Add StoreManagerController and MusicStoreDB.

--- MvcMusicStore/MvcMusicStore/Global.asax.cs
+++ MvcMusicStore/MvcMusicStore/Global.asax.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore
 {
@@ -12,6 +15,7 @@ namespace MvcMusicStore
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MusicStoreDbInitializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
